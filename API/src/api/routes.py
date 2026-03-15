7acbada0a1fa6b5f7bae8fea595248f4d87a86fc
j0lyn3 <evergloam1212@outlook.com>
2026-03-15 14:28:42 -0400

    added rsa endpoints; NEED AUTH MIDDLEWATE

--- API/src/api/routes.py
+++ API/src/api/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from ..redis_db.msg import read_msg, write_msg
+from ..redis_db import msg, user
 
 router = APIRouter()
 
+"""
 @router.get("/msg")
 async def get_msg_test(msg: str):
     await write_msg(msg)
@@ -11,4 +12,12 @@ async def get_msg_test(msg: str):
 @router.get("/msg/read")
 async def get_msg_read():
     return await read_msg()
+"""
 
+@router.get("/rsa/{uid}")
+async def get_rsa_pub():
+    return True
+
+@router.post("/rsa/{uid}")
+async def post_rsa_pub():
+    return True
