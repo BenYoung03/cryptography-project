f1acbbbc1be076ca464a906711925cb3aa2726a7
j0lyn3 <evergloam1212@outlook.com>
2026-03-15 14:30:21 -0400

    added different payload type logic

--- API/src/websocket/ws.py
+++ API/src/websocket/ws.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+import asyncio
 from .manager import ConnectionManager
-from .models import wsMessage
+from .models import wsMessage, wsError
+from ..models.msg import Msg, Update
 from ..redis_db.redis import redis_client as r
-import json
+from ..redis_db import msg as msgCRUD
 
 router = APIRouter()
 manager = ConnectionManager()
@@ -16,9 +18,16 @@ async def ws_endpoint(ws: WebSocket, uid: str):
             data = await ws.receive_json()
             
             message = wsMessage(**data)
+            if message.type == "msg":
+                message.payload = Msg(**message.payload)
+            if message.type == "update":
+                message.payload = Update(**message.payload)
+                if msgCRUD.get_msg_recipient(message.payload.msg_id) != uid: 
+                    asyncio.create_task(manager.send_to_user(uid, wsMessage("error", wsError("no-access", "message to update does not belong to client"))))
+                    continue
             await r.publish(
                 "ws_events",
-                message.model_dump_json()
+                message
             )
     except WebSocketDisconnect:
         await manager.disconnect(uid)
