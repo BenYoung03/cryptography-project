5e15db750148a72f266fad13ce6df42ff88601e2
j0lyn3 <evergloam1212@outlook.com>
2026-03-15 21:30:34 -0400

    Finsihed RSA get and added historical data endpoint

--- API/src/api/routes.py
+++ API/src/api/routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from ..redis_db import user
+from ..redis_db import msg
 from ..models.user import PubRSA
 from ..middleware.auth import get_uid
+import time
 
 router = APIRouter()
 
@@ -20,6 +22,9 @@ async def get_msg_read():
 @router.get("/rsa")
 async def get_rsa_pub(uid: str = Depends(get_uid)):
     res = await user.get_user_rsa(uid)
+    if res == None:
+        return JSONResponse(status_code=404, content={"error": "uid does not have associated public key"})
+    return JSONResponse(status_code=200, content={uid: res})
 
 @router.post("/rsa")
 async def post_rsa_pub(rsa: PubRSA, uid: str = Depends(get_uid)): ## fastAPI rejects if body doesnt match this!! very handy
@@ -27,3 +32,9 @@ async def post_rsa_pub(rsa: PubRSA, uid: str = Depends(get_uid)): ## fastAPI rej
     if res<0:
         return JSONResponse(status_code=500, content={"error": "redis update/write failed, contact backend dev"})
     return JSONResponse(status_code=201, content={"success": f"New RSA pub key written for {uid}"})
+
+@router.get("/history")
+async def get_history(ts: int = time.time(), uid: str = Depends(get_uid)):
+    messages = msg.get_messages_since(uid, ts)
+    updates = msg.get_updates_since(uid, ts)
+    return JSONResponse(status_code=200, content={"messages": messages, "updates": updates, "since": ts})
