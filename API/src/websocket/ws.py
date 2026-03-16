065a86bffc56a038463e97034893eccbd3e479cb
j0lyn3 <evergloam1212@outlook.com>
2026-03-15 20:38:26 -0400

    added uid grabbing from auth model; updated message flow

--- API/src/websocket/ws.py
+++ API/src/websocket/ws.py
@@ -1,33 +1,46 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 import asyncio
-from .manager import ConnectionManager
-from ..models.ws import wsMessage, wsError
-from ..models.msg import Msg, Update
+
+## Backend Managers and DB
 from ..redis_db.redis import redis_client as r
 from ..redis_db import msg as msgCRUD
+from .manager import ConnectionManager
+
+## Data Models
+from ..models.ws import wsMessage, wsError
+from ..models.msg import Msg, Update
+
+## utility functions
 from ..utils.ulid import generate_id
+from ..middleware.auth import get_uid
 
 router = APIRouter()
 manager = ConnectionManager()
 
 @router.websocket("/ws")
-async def ws_endpoint(ws: WebSocket, uid: str):
-    await manager.connect(ws, uid)
+async def ws_endpoint(ws: WebSocket, uid: str = Depends(get_uid)):
+    await manager.connect(ws, uid) ## add connection to manager
 
     try:
         while True:
             data = await ws.receive_json()
             
-            message = wsMessage(**data)
+            message = wsMessage(
+                type=data["type"],
+                payload=data["payload"]
+            )
+
             if message.type == "msg":
-                message.payload = Msg(msg_id=generate_id('m'), **message.payload)
-                msgCRUD.store_message(message.payload)
+                message.payload = Msg(**message.payload)
+                message.payload.msg_id = generate_id("m")
+                await msgCRUD.store_message(message.payload)
+
             elif message.type == "update":
                 message.payload = Update(**message.payload)
                 if msgCRUD.get_msg_recipient(message.payload.msg_id) != uid: 
-                    asyncio.create_task(manager.send_to_user(uid, wsMessage("error", wsError("no-access", "message to update does not belong to client"))))
+                    asyncio.create_task(manager.send_to_user(uid, wsMessage("error", wsError("no-access", "message does not belong to client"))))
                     continue
-                msgCRUD.update_message_status(**message.payload)
+                msgCRUD.update_message_status(message.payload)
             await r.publish(
                 "ws_events",
                 message.model_dump_json()
