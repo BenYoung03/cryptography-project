8d4433d4508ec54b486e1d2f03bee642e2d0c087
j0lyn3 <evergloam1212@outlook.com>
2026-03-16 14:08:50 -0400

    updated and tested rsa endpoints

--- API/src/api/routes.py
+++ API/src/api/routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, Request, Header
+from fastapi import APIRouter, Depends, Request, Header, Response
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from ..redis_db import user
 from ..redis_db import msg
 from ..models.user import PubRSA
@@ -8,24 +9,31 @@ import time
 
 router = APIRouter()
 
+class RSAResponse(BaseModel):
+    uid: str
+    key: PubRSA
+
 async def required_headers(x_uid: str = Header(...), x_jwt: str = Header(...)):
     # This dependency is just for docs
     return {"uid": x_uid, "jwt": x_jwt}
 
-@router.get("/rsa", dependencies=[Depends(get_uid), Depends(required_headers)])
-async def get_rsa_pub(request: Request):
-    uid = request.state.uid
+@router.get("/{uid}/rsa", dependencies=[Depends(required_headers)])
+async def get_rsa_pub(uid: str) -> Response:
     res = await user.get_user_rsa(uid)
     if res == None:
         return JSONResponse(status_code=404, content={"error": "uid does not have associated public key"})
-    return JSONResponse(status_code=200, content={uid: res})
+    return JSONResponse(status_code=200, content=RSAResponse(uid=uid, key=res).model_dump())
 
-@router.post("/rsa")
-async def post_rsa_pub(rsa: PubRSA, uid: str = Depends(get_uid)): ## fastAPI rejects if body doesnt match this!! very handy
-    res = await user.set_user_rsa(uid, rsa)
+@router.post("/{uid}/rsa", dependencies=[Depends(required_headers)])
+async def post_rsa_pub(uid: str, rsa: PubRSA, x_uid: str = Header(...)) -> Response: ## fastAPI rejects if body doesnt match this!! very handy
+    if uid != x_uid:
+        return JSONResponse(status_code=401, content={"unauthorized": "Can only post keys for authourized user"})
+    
+    res = await user.set_user_rsa(x_uid, rsa)
     if res<0:
-        return JSONResponse(status_code=500, content={"error": "redis update/write failed, contact backend dev"})
-    return JSONResponse(status_code=201, content={"success": f"New RSA pub key written for {uid}"})
+        return JSONResponse(status_code=500, content={"error": "redis update/write failed, contact backend dev; bad!!"})
+    
+    return JSONResponse(status_code=201, content={"success": f"New RSA pub key written for {x_uid}"})
 
 @router.get("/history")
 async def get_history(ts: int = time.time(), uid: str = Depends(get_uid)):
